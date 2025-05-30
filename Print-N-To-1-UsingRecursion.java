e76a3e11c8f7d114d34850d4c888762b8c61efe0
Saksham Patwal <135833794+Saks29@users.noreply.github.com>
2025-05-30 09:24:07 +0530

    Create Print-N-To-1-UsingRecursion.java

---
+++ Print-N-To-1-UsingRecursion.java
@@ -0,0 +1,13 @@
+// Q,Print numbers from N to 1 (space separated) without the help of loops.
+
+class Solution {
+
+    void printNos(int N) {
+        // code here
+        if( N < 1){
+            return;
+        }
+        System.out.print(N+" ");
+        printNos(N-1);
+    }
+}
