64e0e8b7fbde2dbebef14fd3fe887702ea9afe76
Saksham Patwal <135833794+Saks29@users.noreply.github.com>
2025-06-05 17:20:19 +0530

    Update Bubble-sort-GFG.java

--- Bubble-sort-GFG.java
+++ Bubble-sort-GFG.java
@@ -6,12 +6,17 @@ class Solution {
         // code here
         int n = arr.length;
         for(int i = n-1 ; i > 0 ; i--){
+            int SwapCount = 0;
             for(int j = 0 ; j < i ; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    SwapCount ++;
                 }
+            }
+            if(SwapCount==0){
+                break;
             }
         }
     }
