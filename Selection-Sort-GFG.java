3ffbfe706fd995fba9421ee75a924c37dfb5c0bd
Saksham Patwal <135833794+Saks29@users.noreply.github.com>
2025-06-05 17:13:58 +0530

    Create Selection-Sort-GFG.java

---
+++ Selection-Sort-GFG.java
@@ -0,0 +1,20 @@
+//Q.Given an array arr, use selection sort to sort arr[] in increasing order.
+
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        int n = arr.length;
+        for(int i = 0 ; i < n-1 ; i++ ){
+            int temp = 0;
+            int min = i;
+            for(int j = i+1 ; j < n ; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
