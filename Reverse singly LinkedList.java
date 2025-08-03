88e32c1ceda1e182d19ae4fbeae3e38a666d4efa
Saksham Patwal <135833794+Saks29@users.noreply.github.com>
2025-08-03 22:43:20 +0530

    Create Reverse singly LinkedList.java

---
+++ Reverse singly LinkedList.java
@@ -0,0 +1,30 @@
+// Q.Given the head of a singly linked list, reverse the list, and return the reversed list. 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode previousnode = null;
+        ListNode currentnode = head;
+        ListNode nextnode = null;
+
+        while(currentnode != null){
+            nextnode = currentnode.next;
+            currentnode.next = previousnode;
+            previousnode = currentnode;
+            currentnode = nextnode;
+        }
+
+        return previousnode;
+    }
+}
