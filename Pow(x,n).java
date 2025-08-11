34ab0534f13a6a015c5fe6ef28909a3c3ddc6efe
Saksham Patwal <135833794+Saks29@users.noreply.github.com>
2025-08-11 20:56:50 +0530

    Create Pow(x,n).java

---
+++ Pow(x,n).java
@@ -0,0 +1,7 @@
+// Q.Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+class Solution {
+    public double myPow(double x, int n) {
+        double answer = Math.pow(x,n);
+        return answer;
+    }
+}
